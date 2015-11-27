4c4138576a9d32abea8ba9d3eed882658f2107a2
shafikul <sagar.shunno@gmail.com>
2015-11-27 19:08:20 +0600

    Updated Internationalization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+    @user = User.find(user_params)
 
     respond_to do |format|
       if @user.save
